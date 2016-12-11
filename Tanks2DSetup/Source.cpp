1d841c72d4462a59814e5cc2f30668ffbf4e8787
Verciuc Sergiu <v_sergiu2009@yahoo.com>
2016-12-12 00:00:09 +0200

    rezolvare bug 1.0

---
+++ Tanks2DSetup/Source.cpp
@@ -0,0 +1,27 @@
+#include "SFML\Graphics.hpp"
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+
+	RenderWindow renderWindow(VideoMode(640, 480), "Tanks");
+	cout << "window is open!\n";
+	//renderWindow.setPosition(Vector2i(10, 50));
+	while (renderWindow.isOpen())
+	{
+		renderWindow.clear();
+		renderWindow.display();
+		Event event;
+		while (renderWindow.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				renderWindow.close();
+				cout << "window is closed!\n";
+			}
+		}
+	}
+}

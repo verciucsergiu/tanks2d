d2f2909560102ad03b3ca4027fd4630f249c9e73
Verciuc Sergiu <v_sergiu2009@yahoo.com>
2016-12-12 00:54:33 +0200

    no more resize

--- Tanks2DSetup/Source.cpp
+++ Tanks2DSetup/Source.cpp
@@ -6,22 +6,32 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-
-	RenderWindow renderWindow(VideoMode(640, 480), "Tanks");
+	//WINOW W/O RESIZE MODE
+	sf::RenderWindow renderWindow(sf::VideoMode(800, 600), "Tanks", sf::Style::Titlebar | sf::Style::Close);
 	cout << "window is open!\n";
 	//renderWindow.setPosition(Vector2i(10, 50));
+	
+	sf::Clock clock;
+
+	renderWindow.setFramerateLimit(30);
+
 	while (renderWindow.isOpen())
 	{
-		renderWindow.clear();
-		renderWindow.display();
-		Event event;
+		
+		sf::Event event;
 		while (renderWindow.pollEvent(event))
 		{
-			if (event.type == Event::Closed)
+			if (event.type == sf::Event::Closed)
 			{
 				renderWindow.close();
 				cout << "window is closed!\n";
 			}
 		}
+		sf::Time time = clock.getElapsedTime();
+		cout <<1.0f / time.asSeconds() << '\n';
+		clock.restart().asSeconds();
+
+		renderWindow.clear();
+		renderWindow.display();
 	}
 }
